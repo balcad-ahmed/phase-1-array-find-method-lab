071fdf58da6d9c6ed1e4df8f81e58fe27dd26be3
moha1503 <balcad2030@gmail.com>
2025-10-06 05:21:09 +0300

    test passed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record) {
+  // find the first game object where result is 'W'
+  const win = record.find(r => r.result === 'W')
+  // if found, return the year string, otherwise undefined
+  return !!win ? win.year : undefined
+}
+
+// Export for Node (in case tests or environments require it)
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { superbowlWin }
+}
